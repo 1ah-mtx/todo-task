354b346a24c2f911bdf328912c28de4f274fee89
yli <y.li.loeran@gmail.com>
2021-02-09 20:08:34 +0000

    added full calendar, reverted to bootstrap 4

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 import './main.html';
 
 // Global Mongo Collection Object in shared folder
@@ -14,4 +15,6 @@ import './templates/createTask.html';
 import './templates/createTask.js';
 import './templates/displayTasks.html';
 import './templates/displayTasks.js';
+import './templates/calendar.html';
+import './templates/calendar.js';
 
