0c14bf46babd6d3d4e9a15f1ef14bf5b1c7bd3f3
yli <y.li.loeran@gmail.com>
2021-02-10 14:29:37 +0000

    completed basic views

--- client/templates/createTask.js
+++ client/templates/createTask.js
@@ -1,17 +1,33 @@
 
-Template.createTask.onCreated(()=>{});
-
 Template.createTask.helpers({
-  
+  getToday: () => { return new Date().toLocaleDateString('en-CA') },
+  getTime: () => { let now = new Date(); return now.getHours() + ':' + now.getMinutes(); },
 });
 
 Template.createTask.events({'submit form' (event, instance) { 
     // we are taking care of this here, no need for browser submit
     event.preventDefault();
 
-    // get our data from the form (the event's target object)
-    let label = event.target.label.value;
+    // get our data from the form (the event's target object) and put it in an object
+    let form = event.target
+    let dateTime = new Date(form.startDate.value)
+    let time = form.startTime.value.split(":")
+    dateTime.setHours(time[0])
+    dateTime.setMinutes(time[1])
+    
+    let newTask = {
+      label: form.label.value,
+      description: form.description.value,
+      startOn: dateTime,
+      duration: 2,
+      done: false,
+    };
+    
+    // update collection
+    TaskList.insert(newTask)
 
-    console.log(label, " submitted!")
+    // reset the form fields
+    event.target.label.value = ""
+    event.target.description.value = ""
   },
 });
---
+++ client/templates/displayTasks.js
@@ -0,0 +1,24 @@
+
+
+// displayTasks 
+Template.displayTasks.helpers({
+    tasks: () => { return TaskList.find() },
+});
+
+
+// tasks
+Template.task.helpers({
+    formatDate: (date) => { return date.toLocaleDateString() },
+    formatTime: (date) => { return date.getUTCHours() + ':' + date.getUTCMinutes()}
+});
+
+Template.task.events({
+    "click input": (event, template) => {
+        let id = template.data._id;
+        let isDone = ! TaskList.findOne({_id: id}).done;
+        TaskList.update({_id: id}, {$set: {done: isDone}});
+    },
+    "click .delete-task": (event, template) => {
+        TaskList.remove({_id: template.data._id});
+    },
+});
