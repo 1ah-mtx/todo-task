2abc4d6195b33869cc1e368bdac3050293f0cc79
yli <y.li.loeran@gmail.com>
2021-02-12 12:13:57 +0000

    added state class to task item display for late

--- client/templates/displayTasks.js
+++ client/templates/displayTasks.js
@@ -2,6 +2,10 @@
     displayTasks scripts
 */
 
+const TASK_CLASS_DONE = 'task-done';
+const TASK_CLASS_LATE = 'task-late';
+const TASK_CLASS_PENDING = 'task-pending';
+
 // task list helpers
 Template.displayTasks.helpers({
     // gets the task list from the collection
@@ -12,7 +16,24 @@ Template.displayTasks.helpers({
 // task item helpers
 Template.task.helpers({
     formatDate: (date) => { return date.toLocaleDateString() },
-    formatTime: (date) => { return ('00' + date.getUTCHours()).slice(-2) + ':' + ('00' + date.getUTCMinutes()).slice(-2)}
+    formatTime: (date) => { return ('00' + date.getUTCHours()).slice(-2) + ':' + ('00' + date.getUTCMinutes()).slice(-2)},
+    // returns class name indicating task completion state
+    state: (done, startDate, duration) => {
+        let state = TASK_CLASS_PENDING;
+        if (done) {
+            state = TASK_CLASS_DONE;
+        }
+        else {
+            let now = new Date();
+            let dueDate = new Date(startDate);
+            dueDate.setTime(dueDate.getTime() + (duration * 60 * 60 * 1000));
+
+            if (dueDate.getTime() < now.getTime()) {
+                state = TASK_CLASS_LATE;
+            }
+        }
+        return state;
+    }
 });
 
 // task item event handlers
